eac3c3eac5a7f7743c56c56fc5e8daeb670f0467
bgn2k <bgn.cbe@gmail.com>
2024-10-19 16:29:18 +0530

    -- Moved components to pages folder --

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import { Home } from "./Home";
-import { Login } from "./Login";
-import { Register } from "./Register";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Dashboard } from "./Dashboard";
+import { Dashboard } from "./pages/Dashboard";
 function App() {
   return (
     <>
